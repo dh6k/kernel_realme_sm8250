17a598a9ae8ac70616907e78024e1be9f3c4131d
Lord Sithek <sithek88@gmail.com>
2025-02-15 07:38:21 +0100

    Update zip.sh & anykernel.sh

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Realme GT Neo 2 / Realme X50 Pro B I G S H O T
+kernel.string=Realme GT Neo 2 / Realme X50 Pro B I G S H O T --with-KSU-Next--
 do.devicecheck=0
 do.modules=0
 do.systemless=1
--- zip.sh
+++ zip.sh
@@ -1,7 +1,11 @@
 cp out/arch/arm64/boot/Image ./anykernel/
 cp out/arch/arm64/boot/dtbo.img ./anykernel/
 
+[ -n "$(find ./anykernel -maxdepth 1 -type f -name '*.zip')" ] && rm ./anykernel/*.zip
 
-cd anykernel && zip -r Bigshot-kernel-AOSP-$(date +"%d-%m-%Y"-%H%M).zip * && mv Bigshot-kernel-AOSP-$(date +"%d-%m-%Y-%H%M").zip ../out/
+cd anykernel && zip -r Bigshot-kernel-AOSP-KSU-Next-$(date +"%d-%m-%Y"-%H%M).zip *
+#mv Bigshot-kernel-AOSP-AOSP-KSU-Next-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
 cd ..
+
+rm anykernel/Image && rm anykernel/dtbo.img
